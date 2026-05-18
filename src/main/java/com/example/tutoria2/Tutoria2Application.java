93c79e80c6e03cc5abb4a2304b7021126158903b
PaulaFlorez06 <andre.pf.espinosa@gmail.com>
2026-05-18 13:04:52 -0500

    initial commit

---
+++ src/main/java/com/example/tutoria2/Tutoria2Application.java
@@ -0,0 +1,13 @@
+package com.example.tutoria2;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Tutoria2Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Tutoria2Application.class, args);
+	}
+
+}
---
+++ src/test/java/com/example/tutoria2/Tutoria2ApplicationTests.java
@@ -0,0 +1,14 @@
+package com.example.tutoria2;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+//pruebas unitarias, mide el nivel de covertura del codigo en una escala de 0 a 100 de cada uno de los componentes. 
+@SpringBootTest
+class Tutoria2ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
